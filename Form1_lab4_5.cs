d4b5665bf6fb9392027478e245947392800bbb08
RinaLina <32177669+RinaLina@users.noreply.github.com>
2017-11-22 01:13:51 +0300

    Add files via upload

---
+++ Form1_lab4_5.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace _4_лаба_3_сем
+{
+    public partial class Forest_quest : Form
+    {
+        public Forest_quest()
+        {
+            InitializeComponent();
+        }
+
+        List<string> list = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+              
+                string text = File.ReadAllText(fd.FileName);
+
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+
+                string[] textArray = text.Split(separators);
+
+                foreach (string strTemp in textArray)
+                {
+
+                    string str = strTemp.Trim();
+                    int check = 0;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (EditDistance.Distance(str, list[i]) != 0) check += 1;
+                        else
+                            break;
+                    }
+                    if (check == list.Count) list.Add(str);
+                   // if (!list.Contains(str)) list.Add(str);
+                }
+
+                t.Stop();
+                this.textBoxFileReadTime.Text = t.Elapsed.ToString();
+                this.textBoxFileReadCount.Text = list.Count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+ 
+                string wordUpper = word.ToUpper();
+
+
+                List<string> tempList = new List<string>();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+
+                t.Stop();
+
+                this.textBox1.Text = this.textBox1.Text + word + '-' + t.Elapsed.ToString() + Environment.NewLine;
+
+                word =  word + '-' + t.Elapsed.ToString() + '\n';
+
+
+                this.listBox1.BeginUpdate();
+
+                    this.listBox1.Items.Add(word);
+
+                this.listBox1.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program_lab1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace lab_1
+{
+    public class Complex
+    {
+        public double re;
+        public double im;
+    }
+
+    class Program
+    {
+        static double ReadNumber()
+        {
+            try
+            {
+                double num = Convert.ToDouble(Console.ReadLine());
+                return num;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Совсем не то, попытайся еще раз)");
+                return ReadNumber();
+            }
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Введи коэффициенты квадратного уравнения A, B, C:");
+            Console.WriteLine("А:");
+            double a = ReadNumber();
+            Console.WriteLine("В:");
+            double b = ReadNumber();
+            Console.WriteLine("С:");
+            double c = ReadNumber();
+            double d = b * b - 4 * a * c;
+            if (d >= 0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / 2 * a;
+                double x2 = (-b - Math.Sqrt(d)) / 2 * a;
+                Console.WriteLine("Вот твое решение: x1 = {0}, x2 = {1}", x1, x2);
+            }
+            else
+            {
+                Complex x1 = new Complex();
+                Complex x2 = new Complex();
+                x1.re = -b / 2 * a;
+                x1.im = Math.Sqrt(-d) / 2 * a;
+                x2.re = x1.re;
+                x2.im = -x1.im;
+                Console.WriteLine("Вот твое решение: {0}+{1}i, {2}-{3}i", x1.re, x1.im, x2.re, x2.im);
+            }
+
+        }
+    }
+}
